a5a5ef64e75e292824b76c24108ea94df7402a8a
UNCROOS <135022720+uncroos@users.noreply.github.com>
2025-12-22 20:23:41 +0900

    Update app_colors.dart

---
+++ firststep/lib/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const navy = Color(0xFF1A237E);
+  static const lightGray = Color(0xFFF5F5F5);
+  static const textGray = Color(0xFFBDBDBD);
+}
